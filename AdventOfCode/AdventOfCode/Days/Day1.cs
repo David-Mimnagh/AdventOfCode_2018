e62e2298cf33ec33dee3dd89c819b08e08465659
David Mimnagh <david.mimnagh@verint.com>
2018-12-03 15:36:08 +0000

    Day 1 complete.

---
+++ AdventOfCode/AdventOfCode/Days/Day1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day1
+    {
+        public string[] PuzzleInput { get; set; }
+        public List<int> SeenFrequencies { get; set; }
+        public bool FoundMatch { get; set; }
+        public int CurrentFreq { get; set; }
+        public void CalculateFreq_Part1()
+        {
+            CurrentFreq = 0;
+            
+            foreach (var pI in PuzzleInput)
+            {
+                CurrentFreq -= (Convert.ToInt32(pI) * -1);
+            }
+            Console.WriteLine("Answer: " + CurrentFreq.ToString());
+        }
+        public void CalculateFreq_Part2()
+        {
+            CurrentFreq = 0;
+            SeenFrequencies = new List<int>();
+            SeenFrequencies.Add(CurrentFreq);
+            while (!FoundMatch)
+            {
+                foreach (var pI in PuzzleInput)
+                {
+                    CurrentFreq -= (Convert.ToInt32(pI) * -1);
+                    if (!SeenFrequencies.Contains(CurrentFreq))
+                        SeenFrequencies.Add(CurrentFreq);
+                    else
+                    {
+                        FoundMatch = true;
+                        Console.WriteLine("FOUND FIRST MATCH: " + CurrentFreq.ToString());
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        const int DAYNUMBER = 1;
+        const string BASEPATH = @"C:\Projects\Personal\AoC\AdventOfCode\AdventOfCode\Files\day";
+
+
+        static string[] ReadFileInput()
+        {
+            var currPath = BASEPATH + (DAYNUMBER.ToString() + ".txt");
+            var input = System.IO.File.ReadAllLines(currPath);
+            return input;
+        }
+
+        static void Main(string[] args)
+        {
+            Day1 d1 = new Day1();
+            d1.PuzzleInput = ReadFileInput();
+            d1.CalculateFreq_Part2();
+            Console.ReadLine();
+        }
+    }
+}
